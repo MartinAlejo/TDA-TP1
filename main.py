384a0bbb6abc1f7cdca5f13f09a33ea4348afe9c
MartinAlejo <mpolese@fi.uba.ar>
2024-09-08 19:59:57 -0300

    Se considera que el archivo empieza en la primer linea. No hay skip

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ def juego_monedas(monedas):
     i = 0
     j = len(monedas) - 1
 
-    acum_sofia = 0
+    acum_sophia = 0
     acum_mateo = 0
-    while not i > j:
+    while i < j:
         moneda1 = monedas[i]
         moneda2 = monedas[j]
         if turno % 2 == 0:
             if moneda1 > moneda2:
-                acum_sofia += moneda1
+                acum_sophia += moneda1
                 i += 1
             else:
-                acum_sofia += moneda2
+                acum_sophia += moneda2
                 j -= 1
         else:
             if moneda1 < moneda2:
@@ -27,21 +27,20 @@ def juego_monedas(monedas):
                 j -= 1
         turno += 1
 
-    return acum_sofia
+    return acum_sophia
 
 # Dado un archivo csv, devuelve una lista con las monedas
 def obtener_monedas(path, delimiter):
     monedas = []
     with open(path, "r") as f:
         csv_reader = csv.reader(f, delimiter=delimiter)
-        next(csv_reader) # para saltear el comentario que esta al principio de los ejemplos. TODO: Quiza quitar
         for l in csv_reader:
             monedas = [int(moneda) for moneda in l]
     return monedas
 
 def main(path):
     monedas = obtener_monedas(path, ";")
-    acum_sofia = juego_monedas(monedas)
-    print(acum_sofia)
+    acum_sophia = juego_monedas(monedas)
+    print("Ganancia de Sophia: " + str(acum_sophia))
 
 main("./ejemplos/20.txt")
